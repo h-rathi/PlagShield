19d35fa01408ff2673e44a9947a9521f5ae13117
h-rathi <thehimanshurathi@gmail.com>
2025-04-30 13:08:05 +0530

    reversed changes

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 
 const run= async ()=>{
-     await mongoose.connect('mongodb://mongo:27017/plagshield')
+     await mongoose.connect('mongodb+srv://thehimanshurathi:thehimanshurathi@cluster0.skfs9.mongodb.net/')
      console.log("connected")
 }
 module.exports=run;
